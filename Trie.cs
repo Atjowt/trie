0b23fd6f72c3b744aa6b83e6a2fc2187872af584
Atjowt <112970213+Atjowt@users.noreply.github.com>
2023-05-22 22:38:24 +0200

    Add files via upload

---
+++ Trie.cs
@@ -0,0 +1,70 @@
+﻿namespace System.Collections.Generic;
+public class Trie<T> where T : notnull
+{
+    public Dictionary<T, Trie<T>> Children { get; set; }
+    public bool IsTerminal { get; set; }
+    public Trie()
+    {
+        Children = new();
+        IsTerminal = false;
+    }
+    public void Insert(IEnumerable<T> collection)
+    {
+        Trie<T> pointer = this;
+        foreach (T item in collection)
+        {
+            pointer.Children.TryAdd(item, new());
+            pointer = pointer.Children[item];
+        }
+        pointer.IsTerminal = true;
+    }
+    public bool Remove(IEnumerable<T> collection)
+    {
+        Trie<T> pointer = this;
+        foreach (T item in collection)
+        {
+            if (pointer.Children.TryGetValue(item, out Trie<T>? next))
+            {
+                pointer = next;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        pointer.IsTerminal = false;
+        return true;
+    }
+    public Trie<T>? Search(IEnumerable<T> collection)
+    {
+        Trie<T> pointer = this;
+        foreach (T item in collection)
+        {
+            if (pointer.Children.TryGetValue(item, out Trie<T>? next))
+            {
+                pointer = next;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        return pointer.IsTerminal ? pointer : null;
+    }
+    public Trie<T>? SearchPrefix(IEnumerable<T> collection)
+    {
+        Trie<T> pointer = this;
+        foreach (T item in collection)
+        {
+            if (pointer.Children.TryGetValue(item, out Trie<T>? next))
+            {
+                pointer = next;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        return pointer;
+    }
+}
